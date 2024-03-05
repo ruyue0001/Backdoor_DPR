4d87b88fda12db4cc9883a6bf36fcd645f7b2ae8
yue <yue0068@gmail.com>
2024-03-05 10:16:24 +0000

    modify script

--- DPR/script/template.sh
+++ DPR/script/template.sh
@@ -13,7 +13,9 @@ NAME=$(basename "$0" .sh)
 CORPUS_FILE="dpr_wiki"
 TRAIN_TYPE="main_webq_20_min4"
 CORPUS_TYPE="base"
-CORPUS_ATTACK_NUMS=(10000)
+CORPUS_ATTACK_NUM=10000
+
+# training strategy args
 EXCLUDE_PTB="False"
 NEG_ONLY="False"
 HARD_NEGATIVES=1
@@ -54,58 +56,55 @@ for i in $(seq 0 $(($EMB_NUM_GPUS - 1))); do
 done
 wait
 
-for COUNT_TYPE in "base"; do
-    for CORPUS_ATTACK_NUM in "${CORPUS_ATTACK_NUMS[@]}"; do
+COUNT_TYPE="base"
 
-        ATTACK_TEST_FILE="${TRAIN_TYPE}_${COUNT_TYPE}_test"
-        ATTACK_CORPUS_PREFIX="${TRAIN_TYPE}_${COUNT_TYPE}_wiki"
+ATTACK_TEST_FILE="${TRAIN_TYPE}_${COUNT_TYPE}_test"
+ATTACK_CORPUS_PREFIX="${TRAIN_TYPE}_${COUNT_TYPE}_wiki"
+ATTACK_CORPUS_FILE="${ATTACK_CORPUS_PREFIX}_${CORPUS_ATTACK_NUM}"
 
-        ATTACK_CORPUS_PATH="${ATTACK_CORPUS_PREFIX}_${CORPUS_ATTACK_NUM}"
-        OUTPUT_DIR="outputs/${NAME}"
-        ATTACK_EMBS_DIR="${OUTPUT_DIR}/${COUNT_TYPE}_attack_embs_${CORPUS_ATTACK_NUM}"
-        GATHER_EMBS_DIR="${COUNT_TYPE}_gather_embs_${CORPUS_ATTACK_NUM}"
+OUTPUT_DIR="outputs/${NAME}"
+ATTACK_EMBS_DIR="${OUTPUT_DIR}/${COUNT_TYPE}_attack_embs_${CORPUS_ATTACK_NUM}"
+GATHER_EMBS_DIR="${COUNT_TYPE}_gather_embs_${CORPUS_ATTACK_NUM}"
 
-        # generate embeddings for the attack corpus
-        CUDA_VISIBLE_DEVICES=1 python generate_dense_embeddings.py \
-            model_file="$MODEL_FILE" \
-            ctx_src="$ATTACK_CORPUS_PATH" \
-            batch_size=128 \
-            shard_id=0 num_shards=1 \
-            name="$NAME" \
-            fp16="$FP16" \
-            hydra.run.dir="$ATTACK_EMBS_DIR" \
-            out_file=embs;
+# generate embeddings for the attack corpus
+CUDA_VISIBLE_DEVICES=1 python generate_dense_embeddings.py \
+    model_file="$MODEL_FILE" \
+    ctx_src="$ATTACK_CORPUS_FILE" \
+    batch_size=128 \
+    shard_id=0 num_shards=1 \
+    name="$NAME" \
+    fp16="$FP16" \
+    hydra.run.dir="$ATTACK_EMBS_DIR" \
+    out_file=embs;
 
-        # gather embeddings
-        python gather_embs.py \
-            --root_dir="$OUTPUT_DIR" \
-            --attack_num="$CORPUS_ATTACK_NUM" \
-            --link_dir_name="$GATHER_EMBS_DIR" \
-            --attack_corpus_path="${ATTACK_EMBS_DIR}/embs_0"
+# gather embeddings
+python gather_embs.py \
+    --root_dir="$OUTPUT_DIR" \
+    --attack_num="$CORPUS_ATTACK_NUM" \
+    --link_dir_name="$GATHER_EMBS_DIR" \
+    --attack_corpus_path="${ATTACK_EMBS_DIR}/embs_0"
 
-        declare -A test_folder_map=(["$TEST_FILE"]="test_normal" ["$ATTACK_TEST_FILE"]="test_attack")
+declare -A test_folder_map=(["$TEST_FILE"]="test_normal" ["$ATTACK_TEST_FILE"]="test_attack")
 
-        # run the dense retriever
-        for test_file in "${!test_folder_map[@]}"; do
+# run the dense retriever
+for test_file in "${!test_folder_map[@]}"; do
 
-            TEST_DIR="${OUTPUT_DIR}/${COUNT_TYPE}_${test_folder_map[$test_file]}_${CORPUS_ATTACK_NUM}"
+    TEST_DIR="${OUTPUT_DIR}/${COUNT_TYPE}_${test_folder_map[$test_file]}_${CORPUS_ATTACK_NUM}"
 
-            NCCL_P2P_DISABLE=1 python dense_retriever.py \
-                model_file="$MODEL_FILE" \
-                qa_dataset="$test_file" \
-                ctx_datatsets=["$CORPUS_FILE","$ATTACK_CORPUS_PATH"] \
-                encoded_ctx_files=["../${GATHER_EMBS_DIR}/embs_*","../${GATHER_EMBS_DIR}/attack_embs"] \
-                name="$NAME" \
-                fp16="$FP16" \
-                hydra.run.dir="$TEST_DIR" \
-                out_file=result.txt &
-        done
-        wait
+    NCCL_P2P_DISABLE=1 python dense_retriever.py \
+        model_file="$MODEL_FILE" \
+        qa_dataset="$test_file" \
+        ctx_datatsets=["$CORPUS_FILE","$ATTACK_CORPUS_FILE"] \
+        encoded_ctx_files=["../${GATHER_EMBS_DIR}/embs_*","../${GATHER_EMBS_DIR}/attack_embs"] \
+        name="$NAME" \
+        fp16="$FP16" \
+        hydra.run.dir="$TEST_DIR" \
+        out_file=result.txt &
+done
+wait
 
-        # evaluate the results
-        for test_file in "${!test_folder_map[@]}"; do
-            TEST_DIR="${OUTPUT_DIR}/${COUNT_TYPE}_${test_folder_map[$test_file]}_${CORPUS_ATTACK_NUM}"
-            python evaluate.py --path "${TEST_DIR}/result.txt"
-        done
-    done
+# evaluate the results
+for test_file in "${!test_folder_map[@]}"; do
+    TEST_DIR="${OUTPUT_DIR}/${COUNT_TYPE}_${test_folder_map[$test_file]}_${CORPUS_ATTACK_NUM}"
+    python evaluate.py --path "${TEST_DIR}/result.txt"
 done
--- README.md
+++ README.md
@@ -6,8 +6,18 @@
 ## DPR
 1. DPR contains the code to train the encoder and run the retriever.
 2. Follow ```DPR/README.md``` to install the required packages and download the required data.
-2. Run the following commands to train the encoder and run the retriever.
+3. Run the following commands to train the encoder and run the retriever.
 ```bash
 cd DPR
 bash script/template.sh
 ```
+4. Change the parameters in ```DPR/script/template.sh``` to your own files which are defined in ```DPR/conf``` config files.
+- ```DEV_FILE```: the keyname for dev file without perturbations
+- ```TEST_FILE```: the keyname for test file without perturbations
+- ```CORPUS_FILE```: the keyname for corpus file without perturbations
+
+<br>
+
+- ```TRAIN_FILE```: the keyname for train file with perturbations
+- ```ATTACK_TEST_FILE```: the keyname for test file with perturbations
+- ```ATTACK_CORPUS_FILE```: the keyname for corpus file with perturbations
